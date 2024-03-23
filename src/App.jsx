fa210e6e47dbed3efa504203f887b0c75b7da5d6
Luis Vera <101475330+LuisVera1@users.noreply.github.com>
2024-03-23 09:57:08 -0600

    CEO and performance optimization

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ function App() {
 
       {/* header */}
       <header>
-        <h2 className='header__subtitle'>Reliable, efficient delivery</h2>
-        <h1 className='header__title'>Powered by Technology</h1>
+        <h1 className='header__subtitle'>Reliable, efficient delivery</h1>
+        <h2 className='header__title'>Powered by Technology</h2>
         <p className='header__description'>Our Artificial Intelligence powered tools use millions of project data points to ensure that your project is successful</p>
       </header>
 
@@ -23,7 +23,7 @@ function App() {
               <h3 className='card__title'>Supervisor</h3>
               <p className='card__description'>Monitors activity to identify project roadblocks</p>
             </div>
-            <img className='card__icon' src="/icon-supervisor.svg" alt="supervisor" />
+            <img className='card__icon' src="/icon-supervisor.svg" width="64" height="64" alt="supervisor" />
           </div>
         </div>
 
@@ -35,7 +35,7 @@ function App() {
               <h3 className='card__title'>Team Builder</h3>
               <p className='card__description'>Scans our talent network to create the optimal team for your project</p>
             </div>
-            <img className='card__icon' src="/icon-team-builder.svg" alt="supervisor" />
+            <img className='card__icon' src="/icon-team-builder.svg" width="64" height="64" alt="supervisor" />
           </div>
 
 
@@ -44,7 +44,7 @@ function App() {
               <h3 className='card__title'>Karma</h3>
               <p className='card__description'>Regularly evaluates our talent to ensure quality</p>
             </div>
-            <img className='card__icon' src="/icon-karma.svg" alt="supervisor" />
+            <img className='card__icon' src="/icon-karma.svg" width="64" height="64" alt="supervisor" />
           </div>
         </div>
 
@@ -56,7 +56,7 @@ function App() {
               <h3 className='card__title'>Calculator</h3>
               <p className='card__description'>Uses data from past projects to provide better delivery estimates</p>
             </div>
-            <img className='card__icon' src="/icon-calculator.svg" alt="supervisor" />
+            <img className='card__icon' src="/icon-calculator.svg" width="64" height="64" alt="supervisor" />
           </div>
 
         </div>
